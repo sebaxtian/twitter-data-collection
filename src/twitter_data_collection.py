86554a48e5e0dfa30782c6cdce3e27f09e5b4925
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2020-03-10 17:52:00 -0500

    Give it a try Twint

--- src/twitter_data_collection.py
+++ src/twitter_data_collection.py
@@ -3,4 +3,19 @@
 # and save them into a normalize dataset.
 #
 
+import twint
+
 print('Hello World !!')
+
+
+# Configure
+c = twint.Config()
+c.Username = "sebaxtianbach"
+#c.Search = ["accidente"]
+c.Location = True
+#c.Geo = "3.449599, -76.544300, 1km"
+#c.Format = "ID {id} | Username {username} | Geo {geo} | Tweet {tweet} | Source {source}"
+#c.Near = "Cali"
+
+# Run
+twint.run.Search(c)
