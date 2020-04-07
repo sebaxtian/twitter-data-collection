853e99b45d4bae8f0bbc339588e02fa02f061285
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2020-04-07 14:28:14 -0500

    Colombia Codiv 19 Pipeline added

--- src/twitter_data_collection.py
+++ src/twitter_data_collection.py
@@ -10,12 +10,20 @@ print('Hello World !!')
 
 # Configure
 c = twint.Config()
-c.Username = "sebaxtianbach"
-#c.Search = ["accidente"]
-c.Location = True
-#c.Geo = "3.449599, -76.544300, 1km"
+#c.Username = "sebaxtianbach"
+c.Search = ["accidente", "movilidad"]
+#c.Location = True
+#c.Geo = "3.398547, -76.525841, 1km"
 #c.Format = "ID {id} | Username {username} | Geo {geo} | Tweet {tweet} | Source {source}"
-#c.Near = "Cali"
+c.Near = "Cali"
+c.Limit = 20
+#c.Store_csv = True
+#c.Output = "./src/output/accidentes.csv"
+c.Lowercase = True
+c.Images = True
+c.Since = "2020-01-01"
+c.Count = True
+#c.Format = "ID {id} | Username {username} | Tweet {tweet} | Photos {photos}"
 
 # Run
 twint.run.Search(c)
